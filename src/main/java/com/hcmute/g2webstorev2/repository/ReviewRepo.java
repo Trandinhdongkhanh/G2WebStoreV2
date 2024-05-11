717acf0dbacb8e071918376832d62e918ed84a32
Trandinhdongkhanh <haxagi1307@gmail.com>
2024-05-11 20:19:43 +0700

    update

--- src/main/java/com/hcmute/g2webstorev2/repository/ReviewRepo.java
+++ src/main/java/com/hcmute/g2webstorev2/repository/ReviewRepo.java
@@ -17,7 +17,9 @@ public interface ReviewRepo extends JpaRepository<Review, Integer> {
     boolean existsByCustomerAndProduct(Customer customer, Product product);
     Page<Review> findAllByProduct(Product product, Pageable pageable);
     Page<Review> findAllByProductAndRate(Product product, Integer rate, Pageable pageable);
+    @Query("select count(*) from Review r where r.rate = :rate")
     long countReviewsByRate(Integer rate);
+    @Query("select count(*) from Review r")
     long countReviews();
     @Query("SELECT SUM(r.rate) FROM Review r")
     long sumOfRates();
--- src/main/java/com/hcmute/g2webstorev2/service/ReviewService.java
+++ src/main/java/com/hcmute/g2webstorev2/service/ReviewService.java
@@ -4,7 +4,6 @@ import com.hcmute.g2webstorev2.dto.request.ReviewRequest;
 import com.hcmute.g2webstorev2.dto.response.ProductReviewsRes;
 import com.hcmute.g2webstorev2.dto.response.ReviewResponse;
 import com.hcmute.g2webstorev2.enums.ReviewSortType;
-import org.springframework.data.domain.Page;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ReviewService {
