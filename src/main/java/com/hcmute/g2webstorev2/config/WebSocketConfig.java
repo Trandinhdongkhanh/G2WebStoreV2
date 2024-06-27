a8450a7615af4c9ba7f0afff53a7c1eee4a66c42
Trandinhdongkhanh <haxagi1307@gmail.com>
2024-06-27 19:12:00 +0700

    update

--- src/main/java/com/hcmute/g2webstorev2/config/WebSocketConfig.java
+++ src/main/java/com/hcmute/g2webstorev2/config/WebSocketConfig.java
@@ -18,7 +18,10 @@ import java.util.List;
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .setAllowedOrigins("*")
+                .withSockJS();
     }
 
     @Override
--- src/main/java/com/hcmute/g2webstorev2/controller/ChatController.java
+++ src/main/java/com/hcmute/g2webstorev2/controller/ChatController.java
@@ -11,11 +11,12 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
 public class ChatController {
     private final SimpMessagingTemplate messagingTemplate;
     private final ChatMessageService chatMessageService;
