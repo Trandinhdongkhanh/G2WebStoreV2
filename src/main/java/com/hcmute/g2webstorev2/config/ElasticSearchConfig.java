3d2d57e000390af54cd592dc4b660e5760c6b664
Trandinhdongkhanh <haxagi1307@gmail.com>
2024-07-12 04:38:14 +0700

    deploy to aws

--- src/main/java/com/hcmute/g2webstorev2/config/ElasticSearchConfig.java
+++ src/main/java/com/hcmute/g2webstorev2/config/ElasticSearchConfig.java
@@ -5,7 +5,13 @@ import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +19,22 @@ import org.springframework.context.annotation.Configuration;
 public class ElasticSearchConfig {
     @Bean
     public RestClient getRestClient() {
-        return RestClient.builder(new HttpHost("localhost", 9200)).build();
+        final CredentialsProvider credentialsProvider =
+                new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials("elastic", "1obU0tPD8PHsVcjYq3bA5a9x"));
+
+        RestClientBuilder builder = RestClient.builder(
+                        new HttpHost("test-8d10fa.es.ap-southeast-2.aws.found.io", 443, "https"))
+                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(
+                            HttpAsyncClientBuilder httpClientBuilder) {
+                        return httpClientBuilder
+                                .setDefaultCredentialsProvider(credentialsProvider);
+                    }
+                });
+        return builder.build();
     }
 
     @Bean
